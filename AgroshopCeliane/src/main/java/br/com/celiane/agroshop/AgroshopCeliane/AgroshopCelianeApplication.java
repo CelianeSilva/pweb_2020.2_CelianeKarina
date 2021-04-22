05b6e14c4976c9f9f9196f0346b1d285a3dd343a
CelianeSilva <karinaceliane1@gmail.com>
2021-04-22 16:42:07 -0300

    Iniciando Projeto da Unidade II

---
+++ AgroshopCeliane/src/main/java/br/com/celiane/agroshop/AgroshopCeliane/AgroshopCelianeApplication.java
@@ -0,0 +1,13 @@
+package br.com.celiane.agroshop.AgroshopCeliane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroshopCelianeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroshopCelianeApplication.class, args);
+	}
+
+}
