5f3eea70651c22c24db3c04cc611e325329652de
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2021-04-27 15:03:51 -0300

    Adicionando novo projeto.

---
+++ agroPopShopCeliane/src/main/java/br/com/celiane/agroshop/AgroshopCelianeApplication.java
@@ -0,0 +1,13 @@
+package br.com.celiane.agroshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroshopCelianeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroshopCelianeApplication.class, args);
+	}
+
+}
---
+++ agroPopShopCeliane/src/test/java/br/com/celiane/agroshop/AgroshopCelianeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.celiane.agroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroshopCelianeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
