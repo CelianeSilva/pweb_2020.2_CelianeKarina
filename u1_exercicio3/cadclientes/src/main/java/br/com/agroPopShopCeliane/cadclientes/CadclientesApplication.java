80baa97c04abc102ff52ea04244e1db1ebe08656
CelianeSilva <karinaceliane1@gmail.com>
2021-05-06 21:43:53 -0300

    Organizando cadclientes

---
+++ u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/cadclientes/CadclientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.agroPopShopCeliane.cadclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadclientesApplication.class, args);
+	}
+
+}
