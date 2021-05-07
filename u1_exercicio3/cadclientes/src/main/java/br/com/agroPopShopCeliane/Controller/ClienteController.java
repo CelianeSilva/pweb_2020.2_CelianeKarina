bfe58a4f1271e006e7ac6dde10a912b1a1a583d4
CelianeSilva <karinaceliane1@gmail.com>
2021-05-07 12:09:57 -0300

    Configurando o Controller

--- u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/Controller/ClienteController.java
+++ u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/Controller/ClienteController.java
@@ -1,14 +1,53 @@
 package br.com.agroPopShopCeliane.Controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class ClienteController {
+import br.com.agroPopShopCeliane.models.Cliente;
+import br.com.agroPopShopCeliane.repository.ClienteRepository;
 
-	@RequestMapping("/cadastrarCliente")
-	public String form() {
-		return "cliente\formCliente";
+@RestController
+@RequestMapping (value = "/clientes")
+public class ClienteController{
+	
+	ClienteRepository clienteRepository;
+	
+	public ClienteController (ClienteRepository clienteRepository) {
+		super();
+		this.clienteRepository = clienteRepository;
+	}
+	
+	@PostMapping
+	public Cliente save (@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 		
 	}
+	
+    @GetMapping
+    public List<Cliente> listaTodos(){
+    	return clienteRepository.findAll();
+    	
+    }
+    
+    @PutMapping
+    public Cliente update(@RequestBody Cliente cliente) {
+    	return clienteRepository.save(cliente);
+    	
+     	
+    	
+    }
+   
+    
+    @DeleteMapping ("/{id}")
+    public void apagar(@PathVariable Long id) {
+    	clienteRepository.deleteById(id);
+    }
 }
--- u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/repository/ClienteRepository.java
+++ u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/repository/ClienteRepository.java
@@ -1,5 +1,11 @@
 package br.com.agroPopShopCeliane.repository;
 
-public class ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.agroPopShopCeliane.models.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
