bd98fded537744ac2d4a10a3578704377938e760
CelianeSilva <karinaceliane1@gmail.com>
2021-05-10 15:35:36 -0300

    adicionado dependências do swagger

--- u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/models/Cliente.java
+++ u1_exercicio3/cadclientes/src/main/java/br/com/agroPopShopCeliane/models/Cliente.java
@@ -1,7 +1,21 @@
 package br.com.agroPopShopCeliane.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_clientes")
+
 public class Cliente {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	
 	private String codigo;
 	private String nome;
 	private String genero;
